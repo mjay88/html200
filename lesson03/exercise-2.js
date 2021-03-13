630fab14eeee7c0d37257c0139cafaf2c7827296
Michael W Jarrett <67067893+mjay88@users.noreply.github.com>
2021-03-13 13:44:19 -0600

    Add files via upload

---
+++ lesson03/exercise-2.js
@@ -0,0 +1,5 @@
+// Make a string variable of a sentence with least five words and a period.
+let string = "Now things are getting interesting."
+    // Make an array from that sentence and console.log the third word.
+let array = string.split(" ");
+console.log(array[2]);
